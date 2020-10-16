af584a9010d64cf7278b7d2f2ec25d811f45621f
Diogo Teixeira <diogo_alex9@hotmail.com>
2020-10-16 11:34:17 +0100

    Both charactes show up
    Health bar doesn't represent damage done

---
+++ First_Project/Assets/HealthScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour
+{
+
+    public float health;
+    public Animator anim;
+
+    public Image image;
+    public GameObject character;
+    private bool isDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 1.0f;
+       // image = GameObject.Find("Villain");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DamageHealth(float damage)
+    {
+        if(isDead) {
+            return;
+        }
+
+        health -= damage;
+        image.fillAmount = health;
+
+        if(health <= 0) {
+            //anim.SetTrigger("Dead");
+            isDead = true;
+            StartCoroutine(removeEnemy());
+            WinPlayer();
+        }
+
+        IEnumerator removeEnemy() {
+            yield return new WaitForSeconds(3);
+            character.SetActive(false);
+        }
+
+        void WinPlayer() {
+            //Animator player = GameObject.Find("Hero").GetComponent<Animator>();
+        }
+    }
+}
