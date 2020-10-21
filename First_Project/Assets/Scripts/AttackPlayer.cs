482673ab1a4c97a02e20e2dfd17117b5a03bbc03
Diogo Teixeira <diogo_alex9@hotmail.com>
2020-10-21 19:41:13 +0100

    Game funcitonal like tutorial
    Deleted old makeshift targets

---
+++ First_Project/Assets/Scripts/AttackPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackPlayer : MonoBehaviour
+{
+    private readonly static float ATTACK_DAMAGE = 0.1f;
+
+    // Start is called before the first frame update
+    void Start(){}
+
+    // Update is called once per frame
+    void Update(){}
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Enemy")
+        {
+            collider.gameObject.GetComponent<HealthScript>().DamageHealth(ATTACK_DAMAGE);
+        }
+    }
+}
---
+++ First_Project/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,55 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour
+{
+
+    public Animator anim;
+
+    public Image image;
+    public GameObject character;
+
+    private float health;
+    private bool isDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DamageHealth(float damage)
+    {
+        if(isDead) {
+            return;
+        }
+
+        health -= damage;
+        image.fillAmount = health;
+
+        if(health <= 0) {
+            anim.SetTrigger("Dead");
+            isDead = true;
+            StartCoroutine(removeEnemy());
+            WinPlayer();
+        }
+
+        IEnumerator removeEnemy() {
+            yield return new WaitForSeconds(3);
+            character.SetActive(false);
+        }
+
+        void WinPlayer() {
+            Animator player = GameObject.Find("Player").GetComponent<Animator>();
+            player.SetTrigger("Win");
+        }
+    }
+}
